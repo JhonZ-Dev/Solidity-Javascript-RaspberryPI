152a381de5e91c02068f635909780d0ee42ee2f8
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-19 13:41:25 -0500

    Upadte

--- tesisblock/migrations/2_deploy_tasks.js
+++ tesisblock/migrations/2_deploy_tasks.js
@@ -1,4 +1,4 @@
-const TasksContract = artifacts.requiere("TasksContract");
+const TasksContract = artifacts.require("TasksContract");
 
 module.exports = function(deployer){
     deployer.deploy(TasksContract);
